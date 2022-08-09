9bbc927c569b7c7c20ee6f8c6f587cec898375f4
Barvinko <vlad.barvinko@gmail.com>
2022-08-09 23:24:09 +0300

    added sending of data to server

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const app = express();
 // создаем парсер для данных в формате json
 const jsonParser = express.json();
 
+// // создаем парсер для данных application/x-www-form-urlencoded
+// const urlencodedParser = express.urlencoded({extended: false});
+
 app.get("/", function (request, response) {  
     response.sendFile(__dirname + "/index.html");
 });
@@ -15,29 +18,25 @@ app.use(express.static(__dirname + "/public"));
 //     response.sendFile(__dirname + "/script.js");
 // });
 
-// app.get("/style/style.css", function (request, response) {
-//     response.sendFile(__dirname + "/style/style.css");
-// });
 
-// app.get("/Assets", function (request, response) {
-//     response.sendFile(__dirname + "/Assets");
-// });
 
-// app.post("/user", jsonParser, function (request, response) {
-//     const fs = require("fs");
-//     if(!request.body) return response.sendStatus(400);
-//     console.log(request.body);
-
-//     fs.readFile(request.body.way, "UTF-8", 
-//             function(error,data){
-//                 console.log("Асинхронное чтение файла");
-//                 if (error) {
-//                   response.json(`нема доступу до файлу`);
-//                 }else{
-//                   console.log(data);  // выводим считанные данные
-//                   response.json(`${data}`);
-//                 }
-//     });
-// });
+app.post("/", jsonParser, function (request, response) {
+    //const fs = require("fs");
+    if(!request.body) return response.sendStatus(400);
+    //show data were sended from front
+    console.log(request.body);
+    let data = request.body;
+    response.json(data);
+    // fs.readFile(request.body.way, "UTF-8", 
+    //         function(error,data){
+    //             console.log("Асинхронное чтение файла");
+    //             if (error) {
+    //               response.json(`нема доступу до файлу`);
+    //             }else{
+    //               console.log(data);  // выводим считанные данные
+    //               response.json(`${data}`);
+    //             }
+    // });
+});
    
 app.listen(3000, ()=>console.log("Сервер запущен..."));
--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -35,7 +35,24 @@ selectEl.addEventListener('change', function() {
 
 })
 
-button.addEventListener('click', function() {
-    console.log(this.className);
-    this.classList.toggle('form-submit-button');
-})
+document.getElementById("submit").addEventListener("click", async function (e) {
+    e.preventDefault();
+   // получаем данные формы
+   let registerForm = document.forms["signUp"];
+   let name = registerForm.elements["name"].value;
+   let phone = registerForm.elements["phone"].value;
+   let select = registerForm.elements["select"].value;
+   // конвектируем данные в json
+   let user = JSON.stringify({name: name, phone:phone, planet:select});
+   let request = new XMLHttpRequest();
+   console.log(user);
+   // посылаем запрос на адрес "/user"
+    request.open("POST", "/", true);   
+    request.setRequestHeader("Content-Type", "application/json");
+    request.addEventListener("load", function () {
+       // получаем и парсим ответ сервера
+        let receivedUser = JSON.parse(request.response);
+        console.log(receivedUser);   // ответ сервера
+    });
+    request.send(user);
+});
